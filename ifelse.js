9edc18d8e4f335947ef870e8b526436d314153f1
mahekshivaniii <mahekshivani42@gmail.com>
2023-12-20 20:34:39 +0530

    new files added

---
+++ ifelse.js
@@ -0,0 +1,7 @@
+var a = 90
+if(a >= 33){
+    console.log("PAsss");
+}
+else{
+    console.log("Fail");
+}
---
+++ ifesleladder.js
@@ -0,0 +1,18 @@
+// 90 to 100 - A
+// 70 to 89 -B
+// 50 to 69 - C
+// lower than 50 -  F
+
+var  a = 65
+if(a  >= 90 && a <= 100){
+    console.log("A")
+}
+else if(a >= 70 && a <=89){
+    console.log("B")
+}
+else if(a >= 50 && a <=69){
+    console.log("C")
+}
+else{
+    console.log("F")
+}
