7bb0cd2df8752bf6ce93b9e22d85a123a0640dc7
mahekshivaniii <mahekshivani42@gmail.com>
2023-12-22 21:52:45 +0530

    learning progress

--- templateLiterals.js
+++ templateLiterals.js
@@ -1,2 +1,16 @@
+//Backtick 
 console.log(`Mahek 
 Shivani`)
+
+//conacat by comma(,)
+let a = 67
+ console.log("My age is " , a)
+
+ //concat by add sign(+)
+
+ 
+ console.log("My age is " + a) 
+
+ //concat by $
+  console.log(`My age  is ${a}`)
+ 
