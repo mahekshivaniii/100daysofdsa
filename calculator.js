077032c864d139f9f24f25158aeaef65704b9d44
mahekshivaniii <mahekshivani42@gmail.com>
2023-12-24 01:35:17 +0530

    learning in progress

---
+++ calculator.js
@@ -0,0 +1,15 @@
+function sum(a,b){
+    console.log(a+b)
+}
+
+function sub(a,b){
+    console.log(a-b)
+}
+
+function mul(a,b){
+    console.log(a*b)
+}
+
+function div(a,b){
+    console.log(a/b)
+}
--- main.js
+++ main.js
@@ -1,21 +1,2 @@
-function sum(a,b){
-    console.log(a+b)
-}
-
-function sub(a,b){
-    console.log(a-b)
-}
-
-function mul(a,b){
-    console.log(a*b)
-}
-
-function div(a,b){
-    console.log(a/b)
-}
 
 
-sum(2,3)
-sub(4,2)
-mul(4,5)
-div(6/3)
