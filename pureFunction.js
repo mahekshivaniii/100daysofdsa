d6e8bde738ec849223448224613eea7c12f0aabf
mahekshivaniii <mahekshivani42@gmail.com>
2023-12-27 00:33:32 +0530

    imp notes

--- pureFunction.js
+++ pureFunction.js
@@ -1,4 +1,5 @@
 var num1 = 6
+// this is an impure function because its depend on num1 variable and num1 is an external factor for the add function
 function add(num2)
 {
     console.log(num1+num2)
