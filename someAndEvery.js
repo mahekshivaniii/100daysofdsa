cc403c548b885bb3bbf6adf24153b0bf359eb379
mahekshivaniii <mahekshivani42@gmail.com>
2023-12-30 01:59:38 +0530

    learn every

--- someAndEvery.js
+++ someAndEvery.js
@@ -1,6 +1,6 @@
 //some function
 
-const num = [ 12,4,5,6,9,45,6,73,245];
+const num = [ 12,4,5,-6,9,45,6,73,245];
 
 let checkNum = num.some((n) => {
   return n<0
@@ -10,3 +10,9 @@ console.log(checkNum)
 
 
 //every
+
+let checkNum2 = num.every((nu)=> {
+    return nu>0
+})
+
+console.log(checkNum2)
