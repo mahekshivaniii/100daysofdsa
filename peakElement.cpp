132a8a943ee987a8aa7ffeb03e4a861194e78572
mahekshivaniii <mahekshivani42@gmail.com>
2024-01-01 01:44:58 +0530

    add c++ file

--- peakElement.cpp
+++ peakElement.cpp
@@ -1,1 +1,20 @@
-int arr[4] ={}
+#include<iostream>
+using namespace std;
+int main (){
+    
+   
+ int arr[4] ={5,10,20,15};
+ 
+
+for (int i = 0; i < 4; i++)
+{
+    /* code */
+    if(arr[i] < arr[i+1] && arr[i+1] > arr[i+2]){
+
+    cout<<arr[i+1]<<endl;
+}
+
+
+}
+return 0 ;
+}
