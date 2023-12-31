ecdab9d2be72e2c735588eca26d909e4bdeea9f8
mahekshivaniii <mahekshivani42@gmail.com>
2023-12-31 22:22:47 +0530

    learning in progress

---
+++ chaining.js
@@ -0,0 +1,10 @@
+let arr = [
+    {name:'a',age:45,gender:'m'},
+    {name:'b',age:35,gender:'m'},
+    {name:'c',age:65,gender:'f'},
+    {name:'d',age:55,gender:'f'},
+    {name:'e',age:25,gender:'m'},
+    {name:'f',age:15,gender:'f'},
+    {name:'g',age:30,gender:'m'},
+    {name:'h',age:21,gender:'f'}
+]
