65249b31b46cfcae1c5dc4e2f6ff8182760b2360
mahekshivaniii <mahekshivani42@gmail.com>
2024-01-01 18:23:40 +0530

    2 problem solvef

--- minMax.cpp
+++ minMax.cpp
@@ -1,7 +1,24 @@
 #include<iostream>
+#include<limits.h>
 using namespace std;
 int main(){
 
+    int arr[4] = {340,5,6,67};
+    int min  = INT_MAX ;
+     int max = INT_MIN ;
+    for(int i = 0 ;i<4;i++){
+        if(arr[i] <min){ // 340 < 
+            min= arr[i];
+
+        }
+       
+        if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+
     
+  cout<<"the max value of array is " << max <<endl;
+  cout<<"the min value of array is " << min << endl;
     return 0 ;
 }
