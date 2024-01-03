d3447a29b67433ccd2fb12511dbfc8aa9ddd74ed
Mahek shivani <117032523+mahekshivaniii@users.noreply.github.com>
2024-01-03 21:32:17 +0530

    learn this

--- this.js
+++ this.js
@@ -1,24 +1,47 @@
 // access this keyword in global enivornment 
 
- console.log(this)
+//  console.log(this)
 
 
-//access this keyword in function
+// //access this keyword in function
 
-let a =  () => {
-    console.log(this)
-}
+// let a =  () => {
+//     console.log(this)
+// }
 
-console.log(a)
+// console.log(a)
 
 
-//access this with normal function
+// //access this with normal function
 
-function m(){
-    console.log(this)
-}
+// function m(){
+//     console.log(this)
+// }
 
-m()
+// m()
 
 // access this keyword in object 
 
+// let myObj = {
+//     name :'mahek',
+//     age:45,
+   
+//     myFn : function (){
+//         console.log(this)
+//     }
+// }
+// myObj.myFn()
+
+let myObj2 = {
+    name :'rishika',
+    age:9,
+     
+    myfn2: function(){
+        function myfn3 (){
+            console.log(this)
+        }
+        myfn3()
+    }
+}
+
+myObj2.myfn2()
