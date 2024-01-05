180de6dbea4152499f2fa3d7320de47225441cb4
Mahek shivani <117032523+mahekshivaniii@users.noreply.github.com>
2024-01-05 22:13:01 +0530

    leanr this

--- this3.js
+++ this3.js
@@ -1,12 +1,21 @@
 console.log(this)
 
- function mahek() {
+//  function mahek() {
+//         console.log(this)
+//  }
+// mahek()
+
+// let a =  () => {
+//     console.log(this)
+// }
+
+// console.log(a)
+
+let myObj1 = {
+    name:'mahek',
+    age:45,
+   myfn: function (){
         console.log(this)
- }
-mahek()
-
-let a =  () => {
-    console.log(this)
+    }
 }
-
-console.log(a)
+myobj.myFn()
