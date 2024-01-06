cb1b799acb438f3098224d32d9d7fc8a2bf49308
Mahek shivani <117032523+mahekshivaniii@users.noreply.github.com>
2024-01-06 14:48:07 +0530

    learn this

--- this4.js
+++ this4.js
@@ -14,12 +14,25 @@ console.log(this)
     console.log(m)
 
 
-    let myObj  = {
+    // let myObj  = {
+    //     name:'rishika',
+    //     age:9,
+    //     myFn : function(){
+    //         console.log(this)
+    //     }
+    // }
+
+    // myObj.myFn()
+
+    let myObj2 = {
         name:'rishika',
         age:9,
-        myFn : function(){
-            console.log(this)
+        myfn1:function(){
+            function myfn3(){
+                console.log(this)
+            }
+            myfn3()
         }
     }
-
-    myObj.myFn()
+    
+    myObj2.myfn1()
